2e1c70b25e40f677347d4aa284c06d91a6551421
Vladislav Tankov <vdtankov@gmail.com>
2018-08-03 23:38:01 +0300

    Make name in view var

--- src/main/tanvd/aorm/View.kt
+++ src/main/tanvd/aorm/View.kt
@@ -1,12 +1,17 @@
 package tanvd.aorm
 
+import ru.yandex.clickhouse.ClickHouseUtil
 import tanvd.aorm.query.Query
 
-abstract class View(val name: String) {
+abstract class View(name: String) {
+    var name: String = ClickHouseUtil.escape(name)!!
+
     abstract val query: Query
 }
 
-abstract class MaterializedView(val name: String) {
+abstract class MaterializedView(name: String) {
+    var name: String = ClickHouseUtil.escape(name)!!
+
     abstract val query: Query
     abstract val engine: Engine
 }
