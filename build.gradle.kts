b23374ed95e3e43b432e242c43ef26ea4d13c2e0
Vladislav Tankov <vdtankov@gmail.com>
2018-11-12 13:09:42 +0300

    Release 1.1.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import groovy.lang.GroovyObject
 import org.jfrog.gradle.plugin.artifactory.dsl.PublisherConfig
 
 group = "tanvd.aorm"
-version = "1.1.2-SNAPSHOT"
+version = "1.1.2"
 
 val kotlin_version = "1.3.0"
 
