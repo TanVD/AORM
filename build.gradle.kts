e7e3ceb8fabf06209d00ac28e47eb7601302ab95
denis.linkov <Denis.Linkov@jetbrains.com>
2020-08-20 14:23:57 +0300

    Update jdbc driver version to 0.2.4. Move to kotlin 1.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,8 @@ group = "tanvd.aorm"
 version = "1.1.6-SNAPSHOT"
 
 plugins {
-    kotlin("jvm") version "1.3.61" apply true
-    id("tanvd.kosogor") version "1.0.7"
+    kotlin("jvm") version "1.4.0" apply true
+    id("tanvd.kosogor") version "1.0.10"
 }
 
 repositories {
@@ -15,22 +15,22 @@ repositories {
 
 dependencies {
     api(kotlin("stdlib"))
-    api("ru.yandex.clickhouse", "clickhouse-jdbc", "0.2.3")
-    api("joda-time", "joda-time", "2.10.5")
+    api("ru.yandex.clickhouse", "clickhouse-jdbc", "0.2.4")
+    api("joda-time", "joda-time", "2.10.6")
     api("org.slf4j", "slf4j-api", "1.7.25")
 
-    testCompile("org.junit.jupiter", "junit-jupiter-api", "5.6.0")
-    testRuntime("org.junit.jupiter", "junit-jupiter-engine", "5.6.0")
+    testCompile("org.junit.jupiter", "junit-jupiter-api", "5.6.2")
+    testRuntime("org.junit.jupiter", "junit-jupiter-engine", "5.6.2")
 
-    testCompile("org.testcontainers", "testcontainers", "1.12.5")
-    testCompile("org.testcontainers", "junit-jupiter", "1.12.5")
+    testCompile("org.testcontainers", "testcontainers", "1.14.3")
+    testCompile("org.testcontainers", "junit-jupiter", "1.14.3")
 }
 
 tasks.withType<KotlinJvmCompile> {
     kotlinOptions {
         jvmTarget = "1.8"
-        languageVersion = "1.3"
-        apiVersion = "1.3"
+        languageVersion = "1.4"
+        apiVersion = "1.4"
     }
 }
 
