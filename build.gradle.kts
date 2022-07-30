9a19b9842901d90bced5c29baa3b331dedf16a4b
Eduard.Gurskiy <Eduard.Gurskiy@jetbrains.com>
2022-07-30 12:54:10 +0300

    Move to 1.1.12-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.dsl.KotlinJvmCompile
 import tanvd.kosogor.proxy.publishJar
 
 group = "tanvd.aorm"
-version = "1.1.11"
+version = "1.1.12-SNAPSHOT"
 
 plugins {
     kotlin("jvm") version "1.5.32" apply true
